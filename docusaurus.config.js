0f24db42eef9f8da6df172daa6ab93684a588393
yrx <rongxingyang@outlook.com>
2026-04-08 22:26:17 +0800

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'AI Builder',
+  title: 'AI Builder1',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
